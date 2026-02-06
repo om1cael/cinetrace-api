14323978f4a1efe01bc4cf3f2f4c75e841ad3759
om1cael <188113439+om1cael@users.noreply.github.com>
2026-02-06 16:35:54 -0300

    feat: do not create review if movie doesn't exist

--- src/main/java/com/cinetrace/api/services/ReviewService.java
+++ src/main/java/com/cinetrace/api/services/ReviewService.java
@@ -1,11 +1,13 @@
 package com.cinetrace.api.services;
 
+import com.cinetrace.api.clients.TMDBClient;
 import com.cinetrace.api.dto.ReviewDTO;
 import com.cinetrace.api.dto.ReviewResponseDTO;
 import com.cinetrace.api.models.ReviewModel;
 import com.cinetrace.api.repositories.ReviewRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,7 +18,17 @@ public class ReviewService {
     @Autowired
     private ReviewRepository repository;
 
+    @Autowired
+    private TMDBClient tmdbClient;
+
+    @Value("${API_KEY}")
+    private String apiKey;
+
     public ReviewResponseDTO createReview(ReviewDTO reviewDTO) {
+        if(tmdbClient.getMovieById("Bearer " + apiKey, reviewDTO.movieId()) == null) {
+            throw new EntityNotFoundException();
+        }
+
         ReviewModel reviewModel = new ReviewModel(reviewDTO);
         ReviewModel created = repository.save(reviewModel);
 
