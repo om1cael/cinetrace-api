2b786f7e4a7808fd72db5428452aeb033ded7d1b
om1cael <188113439+om1cael@users.noreply.github.com>
2026-02-06 15:25:32 -0300

    refactor: rename TMDBPopularDTO to TMDBMovieList

--- src/main/java/com/cinetrace/api/clients/TMDBClient.java
+++ src/main/java/com/cinetrace/api/clients/TMDBClient.java
@@ -1,7 +1,7 @@
 package com.cinetrace.api.clients;
 
 import com.cinetrace.api.dto.MovieDTO;
-import com.cinetrace.api.dto.TMDBPopularDTO;
+import com.cinetrace.api.dto.TMDBMovieList;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 @FeignClient(name = "tmdbClient", url = "https://api.themoviedb.org/3")
 public interface TMDBClient {
     @GetMapping("/movie/popular")
-    TMDBPopularDTO getPopularMovies(@RequestHeader("Authorization") String authorization);
+    TMDBMovieList getPopularMovies(@RequestHeader("Authorization") String authorization);
 
     @GetMapping("/movie/{id}")
     MovieDTO getMovieById(@RequestHeader("Authorization") String authorization, @PathVariable("id") Long id);
